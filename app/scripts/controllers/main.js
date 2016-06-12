01995edc330122f1c69a01590d79609ad9e6bf98
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-06-13 01:40:19 +0800

    Modify and replace existing code with a .todos.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      $scope.todos = ["Item 1", "Item 2", "Item 3", "Item 1"];
   });
