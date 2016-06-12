fe3fe68d9a22213490e46f89512f61ef41fbaeb8
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-06-13 02:02:44 +0800

    Added code to bind the onsubmit event to addTodo() function.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,9 @@
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
       $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+      
+      $scope.addTodo = function () {
+          $scope.todos.push($scope.todo);
+          $scope.todo = '';
+      };
   });
